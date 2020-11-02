e1805669cedcde85384601ec1a5d583c1821227e
korsicanka <nadezhda.likholet@gmail.com>
2020-11-02 13:08:15 +0300

    change for to while

--- script.js
+++ script.js
@@ -16,7 +16,21 @@ const PersonalMovieDB = {
 // PersonalMovieDB.movies[a] = b;
 // PersonalMovieDB.movies[c] = d;
 
-for (let i = 0; i < 2; i++) 
+// for (let i = 0; i < 2; i++) 
+// { let a = prompt('Один из последних просмотренных фильмов?',''),
+//       b = prompt('На сколько его оцените?','');  
+      
+//       if (a != null && b!=null && a !='' && b!='' &&  a.length < 50) {
+//         PersonalMovieDB.movies[a] = b;
+//         console.log("Good work");
+//       } else {
+//           console.log("Wrong answer");
+//           i--;
+//       }
+// }
+
+let i =0;
+while (i < 2) 
 { let a = prompt('Один из последних просмотренных фильмов?',''),
       b = prompt('На сколько его оцените?','');  
       
@@ -27,7 +41,9 @@ for (let i = 0; i < 2; i++)
           console.log("Wrong answer");
           i--;
       }
+      i++;
 }
+
 
 if (PersonalMovieDB.count < 10) {
     console.log("Вы посмотрели достатчно мало фильмов");
