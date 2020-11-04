a3d33b2cf1590c00541bbda2d053925143c8ef0c
korsicanka <nadezhda.likholet@gmail.com>
2020-11-04 12:22:27 +0300

    add method RememberMyFilms

--- script.js
+++ script.js
@@ -16,29 +16,9 @@ let PersonalMovieDB = {
     movies: {},
     actors: {},
     genres: {},
-    privat: false
+    privat: false,
+    MyFilms:RememberMyFilms()
 };
-
-// const a = prompt('Один из последних просмотренных фильмов?',''),
-//       b = prompt('На сколько его оцените?',''),
-//       c = prompt('Один из последних просмотренных фильмов?',''),
-//       d = prompt('На сколько его оцените?','');
-
-// PersonalMovieDB.movies[a] = b;
-// PersonalMovieDB.movies[c] = d;
-
-// for (let i = 0; i < 2; i++) 
-// { let a = prompt('Один из последних просмотренных фильмов?',''),
-//       b = prompt('На сколько его оцените?','');  
-      
-//       if (a != null && b!=null && a !='' && b!='' &&  a.length < 50) {
-//         PersonalMovieDB.movies[a] = b;
-//         console.log("Good work");
-//       } else {
-//           console.log("Wrong answer");
-//           i--;
-//       }
-// }
 
 function RememberMyFilms() {
     let i =0;
